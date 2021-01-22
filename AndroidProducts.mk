a6ca0ec57aed792b2f6dbbf8496fa9dccaee013b
Subhadeep354 <77846617+Subhadeep354@users.noreply.github.com>
2021-01-22 21:58:33 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_RMX1901.mk
+    $(LOCAL_DIR)/potato_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_RMX1901-userdebug \
-    ancient_RMX1901-eng
+    potato_RMX1901-userdebug \
+    potato_RMX1901-eng
